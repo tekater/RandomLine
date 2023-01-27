79abfe1ed19a6d981ccd1fe1840dbf03cc4b93f5
tekater <120014627+tekater@users.noreply.github.com>
2023-01-28 00:43:33 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите size & seed:");
+        int size = scanner.nextInt();
+        int seed = scanner.nextInt();
+
+        Random random = new Random(seed);
+
+        for (int i = 0; i < size; i++) {
+            System.out.println();
+            for (int y = 0; y < size; y++) {
+                if (random.nextBoolean() == true) {
+                    System.out.print("O");
+                }else if (random.nextBoolean() == false) {
+                    System.out.print(" ");
+                }
+            }
+        }
+    }
+}
